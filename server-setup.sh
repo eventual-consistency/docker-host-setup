dfc2de3b1e3e59f4d9bfb32a9aa1fa1d9ebb0726
Steve Gray <steve-gray@users.noreply.github.com>
2017-03-21 20:08:10 +1000

    Create server-setup.sh

---
+++ server-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install the Docker Engine
+apt-get install curl -y
+curl https://get.docker.com/ | sh
+
+# Set storage driver
+tee /etc/docker/daemon.json <<EOF
+{
+        "storage-driver": "overlay2"
+}
+EOF
+
+# Start Docker
+systemctl enable docker
+systemctl start docker
+
+# Run Rancher
+docker run -d -v /data/rancher/mysql:/var/lib/mysql --restart=unless-stopped -p 8080:8080 rancher/server
